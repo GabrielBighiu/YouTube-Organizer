2ebb4ef6d620055845dea61f33298254329b72bc
g4m34x <arteni.george.daniel@gmail.com>
2021-07-29 17:52:49 +0300

    reworked the json save logic as it was not executing as expected

--- method_1.py
+++ method_1.py
@@ -18,23 +18,25 @@ class YTdownloader():
         else:
             self.watched_vids = {}
 
+        self.links_to_dwnld = []
+
     def analyze_input_list(self):
         with open('input_list.txt', 'r') as input_list_handle:
-            self.all_input_rows = input_list_handle.readlines()
+            self.all_input_rows = [row.strip() for row in input_list_handle.readlines()]
 
         for row_id, row_data in enumerate(self.all_input_rows):
-            to_parse = row_data.strip()
-            if '?v=' in to_parse:
-                video_id = to_parse.split('?v=')[-1]
-            else:
-                video_id = to_parse
+
+            if '?v=' in row_data:
+                video_id = row_data.split('?v=')[-1]
+            else: # else used in case only IDs are provided for a quick sync-up
+                video_id = row_data
 
             if video_id in self.watched_vids.keys():
                 print('ID {} already watched !'.format(video_id))
-                self.all_input_rows.pop(row_id)
             else:
                 print('{} added to watched IDs'.format(video_id))
                 self.watched_vids[video_id] = {'download_time': str(datetime.datetime.now())}
+                self.links_to_dwnld.append(row_data)
 
     def save_on_disk(self):
         with open(os.path.join(cloud_storage_root, 'watched_vids.json'), 'w') as json_file_handle:
@@ -45,16 +47,16 @@ class YTdownloader():
     def download_unwatched_vids(self):
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            for video_link in self.all_input_rows:
+            for video_link in self.links_to_dwnld:
                 while True:
                     try:
-                        ydl.download([video_link.strip()])
+                        ydl.download([video_link])
                         break
                     except:
-                        print('Exception found in {}. Retrying in 10 sec...'.format(video_link.strip()))
+                        print('Exception found in {}. Retrying in 10 sec...'.format(video_link))
                         time.sleep(10)
 
 worker = YTdownloader()
 worker.analyze_input_list()
-worker.save_on_disk()
 worker.download_unwatched_vids()
+worker.save_on_disk()
