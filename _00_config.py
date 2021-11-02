5a41eb6b235c82d14ee847f013a4e86404f0738d
George Daniel Arteni <arteni.george.daniel@gmail.com>
2021-11-02 19:20:47 +0200

    updated the default path

--- _00_config.py
+++ _00_config.py
@@ -1,6 +1,6 @@
 ydl_opts = {'format': '[height <=? 720]'}
 
-cloud_storage_root = r'C:\Users\g4m3rx\OneDrive\backups_laptop'
+cloud_storage_root = r'C:\Users\g4m3rx\OneDrive\backup_auto'
 
 possible_out_paths = [
                         r'C:\propriu\temp\01_new_youtube videos',
