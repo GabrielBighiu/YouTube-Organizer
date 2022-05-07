af2543ae950f16ee0ae5182539b36d09ea3d8c17
George Daniel Arteni <arteni.george.daniel@gmail.com>
2022-05-07 13:19:48 +0300

    added more formats descriptors in the config

--- _00_config.py
+++ _00_config.py
@@ -3,8 +3,17 @@
 # a descriptor can be a single format (usually containing audio+video) OR
 # a list of lists of formats containing both video and audio options
 download_formats_prio = [
-    22, #720p video+audio
-    [[247, 136], [139, 249, 250, 140, 251]]
+    22, # mp4 720p video+audio
+    45, # webm 720p video+audio
+    35, # flv 480p video+audio
+    44, # webm 480p video+audio
+    18, # mp4 360p video+audio
+    34, # flv 360p video+audio
+    43, # webm 360p video+audio
+    [[136], [139, 249, 250, 140, 251]], # mp4 720p video only + any audio
+    [[247], [139, 249, 250, 140, 251]], # webm 720p video only + any audio
+    [[135], [139, 249, 250, 140, 251]], # mp4 480p video only + any audio
+    [[168,218,244,245,246], [139, 249, 250, 140, 251]], # webm 480p video only + any audio
 ]
 
 # the folder where the downloads catalog will be saved
